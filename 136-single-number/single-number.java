875fd14b61ef8b664134a4e030961ca2762edc52
Chirag Sharma <chiragsharma8533@gmail.com>
2026-03-03 21:53:07 +0530

    Time: 1 ms (99.91%) | Memory: 46.8 MB (92.80%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(!set.contains(nums[i])){
-            set.add(nums[i]);
-            }else{
-                set.remove(nums[i]);
-            }
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(!set.contains(nums[i])){
+        //     set.add(nums[i]);
+        //     }else{
+        //         set.remove(nums[i]);
+        //     }
+        // }
+        // for(int ele:set){
+        //     return ele;
+        // }
+        // return 0;
+        //USINT BIT MANUIPULATION no extra sapce;
+        int curr=nums[0];
+        for(int i=1;i<nums.length;i++){
+            curr=curr^nums[i];
         }
-        for(int ele:set){
-            return ele;
-        }
-        return 0;
+        return curr;
+
 
 
     }
