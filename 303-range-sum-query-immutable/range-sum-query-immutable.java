de10042541c17af532ec72af72ce230070989615
Chirag Sharma <chiragsharma8533@gmail.com>
2026-03-12 08:49:45 +0530

    Time: 7 ms (99.79%) | Memory: 47.6 MB (78.87%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,14 +1,11 @@
 class NumArray {
     int[] prefix;
     public NumArray(int[] nums) {
-        int n=nums.length;
-         prefix=new int[n+1];
+        prefix=new int[nums.length];
         prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=nums[i]+prefix[i-1];
-
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
         }
-
         
     }
     
