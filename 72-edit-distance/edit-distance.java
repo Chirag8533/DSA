a38f7bf6f430817ece8852f2d9caaac0a5da877c
Chirag Sharma <chiragsharma8533@gmail.com>
2026-03-18 10:33:21 +0530

    Time: 4 ms (77.94%) | Memory: 47.3 MB (48.03%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int[][] dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return solve(n-1,m-1,word1,word2,dp);
+         
+    }
+    public static int solve(int i,int j,String word1,String word2,int[][] dp){
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(word1.charAt(i)==word2.charAt(j)){
+             return solve(i-1,j-1,word1,word2,dp);
+        }
+        int delete=1+solve(i-1,j,word1,word2,dp);
+        int insert=1+solve(i,j-1,word1,word2,dp);
+        int replace=1+solve(i-1,j-1,word1,word2,dp);
+        return dp[i][j]=Math.min(delete,Math.min(insert,replace));
+
+    }
+}
