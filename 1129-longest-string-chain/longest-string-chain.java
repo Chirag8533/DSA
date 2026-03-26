c21c224ea28b40f9e5c39e2516d83ca97fd426f3
Chirag Sharma <chiragsharma8533@gmail.com>
2026-03-26 09:33:25 +0530

    Time: 118 ms (10.46%) | Memory: 56.5 MB (7.42%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,16 +1,15 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Arrays.sort(words,(a,b)->a.length()-b.length());
         int n=words.length;
+        Arrays.sort(words,(a,b)->a.length()-b.length());
         int[][] dp=new int[n][n+1];
         for(int i=0;i<n;i++){
             for(int j=0;j<=n;j++){
                 dp[i][j]=-1;
             }
         }
-
         return solve(0,-1,words,dp);
-
+        
     }
     public static int solve(int i,int prev,String[] words,int[][] dp){
         if(i==words.length){
@@ -20,29 +19,31 @@ class Solution {
             return dp[i][prev+1];
         }
         int take=0;
-        if(prev==-1 || IsPredesor(words[prev],words[i])){
+        if(prev==-1 || checkPredecessor(words[prev],words[i])){
             take=1+solve(i+1,i,words,dp);
         }
         int nottake=solve(i+1,prev,words,dp);
         return dp[i][prev+1]=Math.max(take,nottake);
     }
-    public static boolean IsPredesor(String s1,String s2){
+    public static boolean checkPredecessor(String s1,String s2){
         if(s2.length()-s1.length()!=1){
             return false;
         }
         int i=0;
         int j=0;
-        while(i<s1.length()&& j<s2.length()){
+        while(i<s1.length() && j<s2.length()){
             if(s1.charAt(i)==s2.charAt(j)){
                 i++;
                 j++;
             }else{
                 j++;
             }
+            
         }
         if(i==s1.length()){
             return true;
         }
         return false;
+
     }
 }
