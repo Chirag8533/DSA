fa678e61bc4d7821fa2cbc11e45d75c2829fbb34
Chirag Sharma <chiragsharma8533@gmail.com>
2026-03-27 17:44:21 +0530

    Time: 50 ms (5.30%) | Memory: 53.8 MB (76.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,50 +1,28 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-    //     int n=text1.length();
-    //     int m=text2.length();
-    //     int[][] dp=new int[n][m];
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<m;j++){
-    //             dp[i][j]=-1;
-
-    //         }
-    //     }
-    //     return solve(0,0,text1,text2,dp);
-    // }
-    // public static int solve(int i,int j,String text1,String text2,int[][] dp){
-    //     if(i==text1.length() || j==text2.length()){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     int take=0;
-    //     if(text1.charAt(i)==text2.charAt(j)){
-    //         take=1+solve(i+1,j+1,text1,text2,dp);
-    //     }
-    //     int nottake=Math.max(solve(i+1,j,text1,text2,dp),solve(i,j+1,text1,text2,dp));
-    //     dp[i][j]=Math.max(take,nottake);
-    //     return dp[i][j];
-    //BOTTOM UP
-    int n=text1.length();
-    int m=text2.length();
-    int[][] dp=new int[n+1][m+1];
-    for(int i=0;i<n;i++){
-        dp[i][0]=0;
-    }
-    for(int j=0;j<m;j++){ 
-        dp[0][j]=0;
-    }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(text1.charAt(i-1)==text2.charAt(j-1)){
-                dp[i][j]=1+dp[i-1][j-1];
-            }else{
-                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=-1;
             }
         }
+        return solve(0,0,s1,s2,dp);
     }
-    return dp[n][m];
+    public static int solve(int i,int j,String s1,String s2,int[][] dp){
+        if(i==s1.length() || j==s2.length()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take=0;
+        if(s1.charAt(i)==s2.charAt(j)){
+            take=1+solve(i+1,j+1,s1,s2,dp);
+        }
+        int nottake=Math.max(solve(i+1,j,s1,s2,dp),solve(i,j+1,s1,s2,dp));
+        dp[i][j]=Math.max(take,nottake);
+        return dp[i][j];
     }
-
 }
