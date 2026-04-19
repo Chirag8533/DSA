96dbe28247e7eabceee1c02fe9d8cbfe7e7f5185
Chirag Sharma <chiragsharma8533@gmail.com>
2026-04-19 09:09:27 +0530

    Time: 4 ms (43.07%) | Memory: 52.5 MB (36.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,20 +1,24 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        boolean[][] visited=new boolean[grid.length][grid[0].length];
+        int row=grid.length;
+        int col=grid[0].length;
+        boolean[][] visited=new boolean[row][col];
         int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    dfs(i,j,grid,visited);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(!visited[i][j]){
+                    if(grid[i][j]=='1'){
+                    dfs(i,j,visited,grid);
                     count++;
+                    }
                 }
             }
+            
         }
         return count;
-        
     }
-    public static void dfs(int i,int j,char[][] grid,boolean[][] visited){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length){
+    public static void dfs(int i,int j,boolean[][] visited,char[][] grid){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length){
             return;
         }
         if(grid[i][j]=='0'){
@@ -24,9 +28,11 @@ class Solution {
             return;
         }
         visited[i][j]=true;
-        dfs(i+1,j,grid,visited);
-        dfs(i,j+1,grid,visited);
-        dfs(i,j-1,grid,visited);
-        dfs(i-1,j,grid,visited);
+        dfs(i+1,j,visited,grid);
+        dfs(i-1,j,visited,grid);
+        dfs(i,j+1,visited,grid);
+        dfs(i,j-1,visited,grid);
+        return;
+
     }
 }
