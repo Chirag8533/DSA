1dc37f4fae1fb2c4a71c1cc54b9e802c3dfbc933
Chirag Sharma <chiragsharma8533@gmail.com>
2026-06-16 10:29:38 +0530

    Time: 4 ms (76.47%) | Memory: 55.2 MB (64.71%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String processStr(String s) {
+        int n=s.length();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(Character.isLowerCase(ch)){
+                sb.append(ch);
+            }else if(ch=='%'){
+                sb.reverse();
+            }else if(ch=='*'){
+                if(sb.length()>0){
+                   sb.deleteCharAt(sb.length()-1);
+                }
+
+            }else if(ch=='#'){
+                sb.append(sb.toString());
+            }
+
+        }
+        String str=sb.toString();
+        return str;
+    }
+}
