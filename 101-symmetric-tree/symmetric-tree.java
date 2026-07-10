43915db6983395c091198f9ff7f515d5dc2d2cbd
Chirag Sharma <chiragsharma8533@gmail.com>
2026-07-11 00:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (16.05%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -22,15 +22,12 @@ class Solution {
         return isMirror(root.left,root.right);
     }
     public boolean isMirror(TreeNode left, TreeNode right){
-
         if(left==null &&right==null){
             return true;
         }
-
         if(left==null ||right==null){
             return false;
         }
-
         if(left.val!=right.val){
             return false;
         }
