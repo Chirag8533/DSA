58f6bb8359a36e0b88e1d0945afd91b359eec0a2
Chirag Sharma <chiragsharma8533@gmail.com>
2026-08-05 23:27:04 +0530

    Time: 56 ms (89.42%) | Memory: 299.2 MB (5.77%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -1,31 +1,31 @@
 class Solution {
 
-    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+    public List<Integer> remainingMethods(int n,int k,int[][] invocations) {
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
 
-        for (int[] edge : invocations) {
+        for(int[] edge:invocations){
             graph.get(edge[0]).add(edge[1]);
         }
 
-        boolean[] suspicious = new boolean[n];
+        boolean[] suspicious=new boolean[n];
 
-        dfs(k, graph, suspicious);
+        dfs(k,graph,suspicious);
 
-        for (int[] edge : invocations) {
+        for(int[] edge:invocations){
 
-            int u = edge[0];
-            int v = edge[1];
+            int u=edge[0];
+            int v=edge[1];
 
-            if (!suspicious[u] && suspicious[v]) {
+            if(!suspicious[u] && suspicious[v]){
 
-                ArrayList<Integer> ans = new ArrayList<>();
+                ArrayList<Integer> ans=new ArrayList<>();
 
-                for (int i = 0; i < n; i++) {
+                for(int i=0;i<n;i++){
                     ans.add(i);
                 }
 
@@ -33,27 +33,26 @@ class Solution {
             }
         }
 
-        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> ans=new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
 
-            if (!suspicious[i]) {
+            if(!suspicious[i]){
                 ans.add(i);
             }
         }
 
         return ans;
     }
+    public void dfs(int node,ArrayList<ArrayList<Integer>> graph,
+                     boolean[] suspicious){
 
-    private void dfs(int node, ArrayList<ArrayList<Integer>> graph,
-                     boolean[] suspicious) {
+        suspicious[node]=true;
 
-        suspicious[node] = true;
+        for(int next:graph.get(node)){
 
-        for (int next : graph.get(node)) {
-
-            if (!suspicious[next]) {
-                dfs(next, graph, suspicious);
+            if(!suspicious[next]){
+                dfs(next,graph,suspicious);
             }
         }
     }
